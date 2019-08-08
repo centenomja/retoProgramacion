9e7e40c9b661d5601cc9d5a3a89d239cabfebf40
Juan Centeno <juancenteno@MacBook-Pro-de-Juan.local>
2019-08-08 15:11:44 -0500

    Initial Commit

---
+++ retoMemoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  retoMemoria
+//
+//  Created by Juan Centeno on 08/08/19.
+//  Copyright © 2019 Juan Centeno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
